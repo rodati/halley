052deec5f16af6bea0ef8b07126bbd1ca42b5dc8
Pablo Ois Lagarde <p@oislagarde.com.ar>
2017-10-05 13:17:40 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,57 @@
+'use strict'
+
+const argv = require('yargs')
+  .option('collections', {
+    describe: 'Collection map YAML file',
+    nargs: 1,
+    default: 'collections.yml',
+    required: true,
+    alias: 'c'
+  })
+
+  .option('sql', {
+    describe: 'SQL server to connect to',
+    nargs: 1,
+    default: 'postgres:///',
+    required: true
+  })
+
+  .option('mongo', {
+    describe: 'Mongo connection string',
+    nargs: 1,
+    default: 'mongodb://localhost',
+    required: true
+  })
+
+  .option('incremental-import', {
+    describe: 'Do an incremental import',
+    boolean: true,
+    alias: 'i'
+  })
+
+  .option('concurrency', {
+    describe: 'PG client concurrency',
+    number: true,
+    default: 1,
+    alias: 'n'
+  })
+
+  .option('delete-mode', {
+    describe: 'How to handle deletes',
+    choices: ['ignore', 'citus-multi-shard', 'normal'],
+    default: 'normal'
+  })
+
+  .option('log', {
+    describe: 'Set the log level',
+    choices: ['error', 'warn', 'info', 'verbose', 'debug', 'silly'],
+    default: 'info',
+    alias: 'l'
+  })
+
+  .help('h')
+  .alias('help', 'h')
+
+  .argv
+
+require('./lib/main')(argv)
