b3973d364e1566e7b72ea1b01401f721ac9653f0
Pablo Ois Lagarde <p@oislagarde.com.ar>
2017-11-01 18:51:43 -0300

    Remove files

--- replicate-1-core.sh
+++
@@ -1,3 +0,0 @@
-node . -c collections-1-core.yml -n 3 \
---mongo "mongodb://cosmo-postgre:ficus@ds061542-a1.mongolab.com:61542,ds061542-a0.mongolab.com:61542/local?replicaSet=rs-ds061542&authSource=cosmo-production&readPreference=secondary" \
---sql "postgres://sirena:d8WFUyycHGdzyonAhyYKzBoV@sirena2.csioptwgy8uh.us-east-1.rds.amazonaws.com:5432/sirena"
--- replicate-2-integrations.sh
+++
@@ -1,3 +0,0 @@
-node . -c collections-2-integrations.yml -n 3 \
---mongo "mongodb://halley-staging:6lTZBVfBqLA0BESc@integrations-shard-00-00-enyvz.mongodb.net:27017,integrations-shard-00-01-enyvz.mongodb.net:27017,integrations-shard-00-02-enyvz.mongodb.net:27017/local?ssl=true&replicaSet=integrations-shard-0&authSource=admin&readPreference=secondary" \
---sql "postgres://sirena:d8WFUyycHGdzyonAhyYKzBoV@sirena2.csioptwgy8uh.us-east-1.rds.amazonaws.com:5432/sirena"
